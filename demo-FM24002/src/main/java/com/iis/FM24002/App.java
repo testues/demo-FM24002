ca3d7b33308081ac62e8e1255b844b8598764344
testues <fm24002@ues.edu.sv>
2024-05-04 18:09:39 -0600

    first commit

---
+++ demo-FM24002/src/main/java/com/iis/FM24002/App.java
@@ -0,0 +1,18 @@
+package com.iis.FM24002;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello World";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println( hw.Hello());
+    }
+}
---
+++ demo-FM24002/src/test/java/com/iis/FM24002/AppTest.java
@@ -0,0 +1,20 @@
+package com.iis.FM24002;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
